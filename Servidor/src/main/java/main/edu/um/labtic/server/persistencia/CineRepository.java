3e8c965db202800cd3c12b2de204c0441d5a06f4
german <gabuarab@correo.um.eduy.uy>
2019-09-04 13:14:23 -0300

    Agrego el repositorio de Cine

---
+++ Servidor/src/main/java/main/edu/um/labtic/server/persistencia/CineRepository.java
@@ -0,0 +1,25 @@
+package main.edu.um.labtic.server.persistencia;
+
+import main.edu.um.labtic.server.negocios.Cine;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CineRepository extends JpaRepository<Cine, Long> {
+}
+/*import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.web.bind.annotation.PostMapping;
+        import org.springframework.web.bind.annotation.RequestBody;
+        import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NoteController {
+    @Autowired
+    NoteRepository noteRepository;
+
+    @PostMapping("/note")
+    public void save(@RequestBody Note note) {
+        noteRepository.save(note);
+    }
+}
+*/
