0c93088907dd20d8b72beef78ce3524b1028982d
german <gabuarab@correo.um.eduy.uy>
2019-09-04 13:04:24 -0300

    Intento de Tabla en Cine

--- Servidor/src/main/java/main/edu/um/labtic/server/negocios/Cine.java
+++ Servidor/src/main/java/main/edu/um/labtic/server/negocios/Cine.java
@@ -1,9 +1,17 @@
 package main.edu.um.labtic.server.negocios;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 @Table(name = "CINE")
 public class Cine {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column(name = "Nombre", nullable = false, length = 40) //nullable false = notblank
+    private String Nombre;
+
 }
